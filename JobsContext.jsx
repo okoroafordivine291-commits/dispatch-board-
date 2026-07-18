d6f5dbc3672e008009f19cfce8795ad50d8c1c3a
Okoroafor Divine <okoroafordivine291@gmail.com>
2026-07-18 10:09:59 +0100

    Update JobsContext.jsx

--- JobsContext.jsx
+++ JobsContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useEffect, useState } from 'react'
-import { fetchJobs } from '../services/api.js'
+import { fetchJobs } from './api.js'
 
 const JobsContext = createContext(null)
 
