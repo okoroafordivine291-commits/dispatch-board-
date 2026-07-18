ddba54dcd5e0c37856a38b06b48c9cce442e261a
Okoroafor Divine <okoroafordivine291@gmail.com>
2026-07-18 10:12:47 +0100

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
-import { JobsProvider } from './context/JobsContext.jsx'
-import { BookmarksProvider } from './context/BookmarksContext.jsx'
+import { JobsProvider } from './JobsContext.jsx'
+import { BookmarksProvider } from './BookmarksContext.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
