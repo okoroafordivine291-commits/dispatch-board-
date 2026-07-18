5798f6a1c93e722bdb239abd9aff515a33962fda
Okoroafor Divine <okoroafordivine291@gmail.com>
2026-07-18 10:10:27 +0100

    Fix import path for useBookmarks in Navbar

--- Navbar.jsx
+++ Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
-import { useBookmarks } from '../context/BookmarksContext.jsx'
+import { useBookmarks } from './BookmarksContext.jsx'
 
 export default function Navbar() {
   const { count } = useBookmarks()
